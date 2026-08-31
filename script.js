bed1caa2f012d9d485a21c8dc2be7c791060eafc
Delaz <fernandesdelazare@gmail.com>
2026-08-31 17:07:16 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -8,3 +8,52 @@ menuBtn.addEventListener("click", function() {
     menu.classList.toggle("active");
 
 });
+// =================================
+// FILTRO DO CARDÁPIO
+// =================================
+
+const filters = document.querySelectorAll(".filter");
+
+const products = document.querySelectorAll(".product-card");
+
+
+filters.forEach(function(filter) {
+
+    filter.addEventListener("click", function() {
+
+        const category = filter.dataset.filter;
+
+
+        // Remove o ativo dos outros botões
+
+        filters.forEach(function(button) {
+
+            button.classList.remove("active");
+
+        });
+
+
+        // Ativa o botão selecionado
+
+        filter.classList.add("active");
+
+
+        // Filtra os produtos
+
+        products.forEach(function(product) {
+
+            if (product.dataset.category === category) {
+
+                product.style.display = "block";
+
+            } else {
+
+                product.style.display = "none";
+
+            }
+
+        });
+
+    });
+
+});
